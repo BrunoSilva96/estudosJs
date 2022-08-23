28e84095f572fd40a65a147abd298219b3f8c6d1
Bruno <brunosilvabrait@gmail.com>
2022-08-23 19:00:22 -0300

    first commit

---
+++ logica-I/exercicio1/cadastro.js
@@ -0,0 +1,11 @@
+let firstName = prompt("Qual o primeiro nome do recruta?");
+let lastName = prompt("Qual o sobrenome do recruta?");
+let fieldStudy = prompt("Qual campo de estudo do recruta?");
+let birth = prompt("Qual o ano de nascimento do recruta?");
+
+alert(
+"Recruta cadastrado com sucesso!\n\n"
+`Nome: ${firstName + lastName},
+Campo de estudo: ${fieldStudy},
+Idade:` + (2022 - birth)
+);
---
+++ logica-I/exercicio1/calculadora.js
@@ -0,0 +1,15 @@
+const numA = Number(prompt("Digite um número:"));
+
+const numB = Number(prompt("Digite mais um número:"));
+
+const soma = numA + numB;
+const subtrair = numA - numB;
+const multiplicar = numA * numB;
+const dividir = numA / numB;
+
+alert(
+`A soma entre os números é: ${soma},
+A subtração entre os números é: ${subtrair},
+A multiplicação entre os números é: ${multiplicar},
+A divisão entre os números é: ${dividir}`
+);
